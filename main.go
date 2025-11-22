1c65faed02326fff241c8046e0d6367be4cec3ab
xterminator24 <scott.mckillop@outlook.com>
2025-11-22 08:04:48 -0400

    create the login feature for the application along with the staging for future commands

--- main.go
+++ main.go
@@ -3,25 +3,79 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/xterminator24/bootdev-blog-gator/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.handlers[cmd.name]
+	if !ok {
+		return fmt.Errorf("unknown command: %s", cmd.name)
+	}
+
+	return handler(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlers[name] = f
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
 
-	err = cfg.SetUser("xterminator24")
-	if err != nil {
-		log.Fatalf("couldn't set current user: %v", err)
+	appState := &state{
+		cfg: &cfg,
 	}
 
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
+	cmds := commands{
+		handlers: make(map[string]func(*state, command) error),
 	}
-	fmt.Printf("Read config again: %+v\n", cfg)
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("not enough arguments")
+	}
+
+	name := os.Args[1]
+	args := os.Args[2:]
+
+	cmd := command{
+		name: name,
+		args: args,
+	}
+
+	if err := cmds.run(appState, cmd); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("you must provide a username")
+	}
+
+	err := s.cfg.SetUser(cmd.args[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("user %v has been logged in\n", s.cfg.CurrentUserName)
+	return nil
 }
