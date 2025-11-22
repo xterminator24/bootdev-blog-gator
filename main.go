9b0e1f05588ff373dbe62f53da980fec591cacac
Scott McKillop <scott.mckillop@outlook.com>
2025-11-22 08:36:27 -0400

    rafactor for better organization and consistency with course solution

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -12,70 +11,30 @@ type state struct {
 	cfg *config.Config
 }
 
-type command struct {
-	name string
-	args []string
-}
-
-type commands struct {
-	handlers map[string]func(*state, command) error
-}
-
-func (c *commands) run(s *state, cmd command) error {
-	handler, ok := c.handlers[cmd.name]
-	if !ok {
-		return fmt.Errorf("unknown command: %s", cmd.name)
-	}
-
-	return handler(s, cmd)
-}
-
-func (c *commands) register(name string, f func(*state, command) error) {
-	c.handlers[name] = f
-}
-
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
 
-	appState := &state{
+	programState := &state{
 		cfg: &cfg,
 	}
 
 	cmds := commands{
-		handlers: make(map[string]func(*state, command) error),
+		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
 
 	if len(os.Args) < 2 {
-		log.Fatal("not enough arguments")
+		log.Fatal("Usage: cli <command> [args...]")
 	}
 
-	name := os.Args[1]
-	args := os.Args[2:]
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
 
-	cmd := command{
-		name: name,
-		args: args,
-	}
-
-	if err := cmds.run(appState, cmd); err != nil {
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) == 0 {
-		return fmt.Errorf("you must provide a username")
-	}
-
-	err := s.cfg.SetUser(cmd.args[0])
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("user %v has been logged in\n", s.cfg.CurrentUserName)
-	return nil
 }
